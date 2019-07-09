1e3b848e4107c6f9e1fb6daa0a06eb043bd07b7b
Kavya Muppalla <kavyamuppalla@gmail.com>
2019-07-09 00:09:24 -0400

    HamletParser

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,10 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -25,6 +29,7 @@ public class HamletParser {
             }
 
             scanner.close();
+
         }catch(IOException e){
             e.printStackTrace();
         }
@@ -34,6 +39,34 @@ public class HamletParser {
 
     public String getHamletData(){
         return hamletData;
+    }
+
+    public List<Integer> findText(String text) {
+
+        List<Integer> indexes = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile(text,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(this.hamletData);
+
+        while(matcher.find()) {
+            indexes.add(matcher.start());
+        }
+
+        return indexes;
+    }
+    public String replaceText(String text, String replaceText) {
+        StringBuilder sb = new StringBuilder();
+        List<Integer> indexs = findText(text);
+        int startIndex = 0;
+        for(int index : indexs) {
+            sb.append(this.hamletData.substring(startIndex, index));
+            sb.append(replaceText);
+            startIndex = index + text.length();
+        }
+        if(startIndex < this.hamletData.length()-1) {
+            sb.append(this.hamletData.substring(startIndex));
+        }
+        return sb.toString();
     }
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,8 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -15,17 +18,32 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        String str = this.hamletParser.replaceText("Hamlet", "Leon");
+        System.out.println(str);
+        Assert.assertFalse(str.contains("Hamlet"));
+        Assert.assertTrue(str.contains("Leon"));
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String str = this.hamletParser.replaceText("Horatio","Tariq");
+        Assert.assertFalse(str.contains("Horatio"));
+        Assert.assertTrue(str.contains("Tariq"));
     }
 
     @Test
     public void testFindHoratio() {
+        List<Integer> list = this.hamletParser.findText("Horatio");
+        Integer expected = 158;
+        Integer actual = list.size();
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
     public void testFindHamlet() {
+        List<Integer> list = this.hamletParser.findText("Hamlet");
+        Integer expected = 472;
+        Integer actual = list.size();
+        Assert.assertEquals(expected,actual);
     }
 }
